46ccbcb92a8ae632f91f13275edff4b8230dd0af
Daniel Schopper <daniel@bitwala.com>
2020-09-01 17:58:35 +0200

    test forked version

--- main.tf
+++ main.tf
@@ -364,7 +364,7 @@ resource "aws_cloudfront_distribution" "default" {
 }
 
 module "dns" {
-  source           = "git::https://github.com/cloudposse/terraform-aws-route53-alias.git?ref=tags/0.8.0"
+  source           = "git::https://github.com/scream314/terraform-aws-route53-alias.git?ref=master"
   enabled          = var.enabled && (var.parent_zone_id != "" || var.parent_zone_name != "") ? true : false
   aliases          = var.aliases
   parent_zone_id   = var.parent_zone_id
